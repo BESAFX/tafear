b0fa10f13a41bc9f071fd665ddf73424853d783b
MahdySoft <islamhaker@gmail.com>
2017-07-12 09:15:02 +0300

    Deploy

--- src/main/java/com/besafx/app/controller/PartialController.java
+++ src/main/java/com/besafx/app/controller/PartialController.java
@@ -1,4 +1,11 @@
 package com.besafx.app.controller;
+import com.besafx.app.entity.Company;
+import com.besafx.app.entity.Person;
+import com.besafx.app.service.PersonService;
+import com.besafx.app.util.JSONConverter;
+import com.besafx.app.util.Options;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
@@ -10,22 +17,35 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
 public class PartialController {
 
+    private final static Logger log = LoggerFactory.getLogger(PartialController.class);
+
     @Autowired
     private SessionRegistry sessionRegistry;
 
+    @Autowired
+    private PersonService personService;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView navToHome() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth.getPrincipal().equals("anonymousUser")) {
+    public ModelAndView navToHome(Principal principal) {
+        if (principal.getName().equals("anonymousUser")) {
             return new ModelAndView("redirect:/login");
         } else {
-            return new ModelAndView("index");
+            ModelAndView modelAndView = new ModelAndView("index");
+            Person person = personService.findByEmail(principal.getName());
+            Options options = JSONConverter.toObject(person.getOptions(), Options.class);
+            if(options.getLang().equals("AR")){
+                modelAndView.addObject("title", "طيف العربية | إدارة المهام");
+            }else{
+                modelAndView.addObject("title", "Tafear | Task Management");
+            }
+            return modelAndView;
         }
     }
 
